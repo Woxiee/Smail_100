82157c7be5e7f3632a29759ec2eab779786115ef
Woxiee <97643020@qq.com>
2018-03-09 17:22:55 +0800

    Initial Commit

---
+++ Smail_100/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Smail_100
+//
+//  Created by ap on 2018/3/9.
+//  Copyright © 2018年 Smail_100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Smail_100/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Smail_100
+//
+//  Created by ap on 2018/3/9.
+//  Copyright © 2018年 Smail_100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
