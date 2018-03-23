8b007fb36593252ca2674a25d3d04d7b2440116d
Woxiee <97643020@qq.com>
2018-03-23 18:31:48 +0800

    2018-3-23
    更新

--- Smail_100/Class/Members(会员)/VCS/OpenBusinss(开通商家)/OpenBusinssVC.m
+++ Smail_100/Class/Members(会员)/VCS/OpenBusinss(开通商家)/OpenBusinssVC.m
@@ -13,9 +13,57 @@
 @end
 
 @implementation OpenBusinssVC
+{
+    __weak IBOutlet UITextField *_accoutTF;
+    __weak IBOutlet UITextField *_nameTF;
+    __weak IBOutlet UITextField *_storeIntoreTf;
+    __weak IBOutlet UITextField *_tellTF;
+    __weak IBOutlet UITextField *_timeTF;
+    
+    __weak IBOutlet UITextView *_mainLineTF;
+    
+    __weak IBOutlet UIView *addressView;
+    
+    __weak IBOutlet UILabel *addressLb;
+    
+    __weak IBOutlet UITextField *_addreDetailTF;
+    
+    __weak IBOutlet UIView *_industryView;
+    
+    __weak IBOutlet UITextField *_industryTf;
+    
+    __weak IBOutlet UIButton *_btn1;
+    __weak IBOutlet UIButton *_btn2;
+    
+    __weak IBOutlet UIButton *_btn3;
+    __weak IBOutlet UIButton *_btn4;
+    
+    __weak IBOutlet UIImageView *_doorImageView;
+    
+    __weak IBOutlet UIImageView *zzImageView;
+    
+    __weak IBOutlet UIImageView *XyImageView;
+    
+    __weak IBOutlet UIImageView *idOnImageView;
+    
+    __weak IBOutlet UIImageView *idDownImageView;
+    
+    __weak IBOutlet UIButton *agrreBnt;
+    
+    __weak IBOutlet UIButton *_xyBtn;
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+
+
+- (IBAction)didClickAgrreAndXXAction:(UIButton *)sender {
+    
+    
 }
 
 
